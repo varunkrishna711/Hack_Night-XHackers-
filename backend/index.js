3a8b020de3305c4db8d2a805d57c72e50b8be520
jaidan22 <jaidansleeba22@gmail.com>
2022-04-16 22:33:38 +0530

    Co-authored-by: Varun Krishna <varunkrishna711@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,6 @@ app.get("/", async (req, res, next) => {
 
 app.use("/api", require("./route"));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log("backend running ..."));
--- tweet_image_new/src/App.jsx
+++ tweet_image_new/src/App.jsx
@@ -2,15 +2,18 @@ import "./App.scss";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useRef } from "react";
+import twitterimg from "./icons8-twitter.svg";
+import html2canvas from "html2canvas";
 
 function App() {
   const [data, setData] = useState({});
   const [id, setId] = useState("");
   const [url, seturl] = useState("");
   const textRef = useRef();
+  const ctnRef = useRef();
 
   let fid = () => {
-    console.log(textRef.current.value);
+    console.log(textRef);
     seturl(textRef.current.value);
     console.log(url);
     setId(textRef.current.value?.split("/")[5]?.split("?")[0]);
@@ -35,6 +38,17 @@ function App() {
       .catch((err) => console.log(err));
   }
 
+  function download(e) {
+    html2canvas(document.querySelector('.container')).then((canvas) => {
+      const base64img = canvas.toDataURL("image/png");
+      let anchor = document.createElement("a");
+      anchor.setAttribute("href", base64img);
+      anchor.setAttribute("download", "twt-img.png");
+      anchor.click();
+      anchor.remove();
+    });
+  }
+
   return (
     <div className="App">
       <div className="navbar">
@@ -42,11 +56,57 @@ function App() {
           id="url"
           type="text"
           placeholder="Paste URL of tweet"
-          // onPaste={fid1}
+          onClick="this.select();"
+          onPaste={(e) => {
+            seturl(e.target.value);
+            // console.log(url);
+            setId(textRef.current.value?.split("/")[5]?.split("?")[0]);
+          }}
+          onChange={(e) => {
+            seturl(e.target.value);
+            // console.log(url);
+            setId(textRef.current.value?.split("/")[5]?.split("?")[0]);
+          }}
           ref={textRef}
         ></input>
-        <button onClick={fid}>submit</button>
+        <button onClick={fid}>&#x27A7;</button>
       </div>
+
+      {data.data && (
+        <>
+          <div className="container" ref={ctnRef}>
+            <div className="card">
+              <div className="head">
+                <div className="left">
+                  <img
+                    id="dp"
+                    src={data.includes.users[0].profile_image_url}
+                    alt="profile"
+                    className="src"
+                  />
+                </div>
+
+                <div className="right">
+                  <div className="name">{data.includes.users[0].name}</div>
+                  <div className="uname">
+                    @{data.includes.users[0].username}
+                  </div>
+                </div>
+                <img id="logo" src={twitterimg} alt="twitter" />
+              </div>
+
+              <div className="body">{data.data.text}</div>
+
+              <div className="footer"></div>
+            </div>
+          </div>
+
+          <button id="dwnld" onClick={(e) => download(e)}>
+            Download
+          </button>
+        </>
+      )}
+      <div className="card"></div>
     </div>
   );
 }
