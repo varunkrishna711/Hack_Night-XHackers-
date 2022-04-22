c8569c30d6fb0d5b835cb5bbab62e8afc9060db6
jaidan22 <jaidansleeba22@gmail.com>
2022-04-22 21:27:51 +0530

    new commit

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
